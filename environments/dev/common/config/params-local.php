1f5105c5e104f7bffdeb51cff3221a88e2e66641
Alexey Vasilyev home <asv2108@gmail.com>
2020-03-25 13:26:01 +0200

    Init one session

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -1,3 +1,8 @@
 <?php
 return [
+'cookieValidationKey' => '',
+'cookieDomain' => '.shop',
+'frontendHostInfo' => 'http://shop',
+'backendHostInfo' => 'http://admin.shop',
+'staticHostInfo' => 'http://static.shop',
 ];
